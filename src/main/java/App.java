b2d79b50c7c5b2d91f7bd8096f9219c14f61770d
JoyWambui <JoyWambui@github.com>
2021-04-22 14:32:13 +0300

    apply styles

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,7 +12,15 @@ import java.util.Map;
 
 
 public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
+    }
     public static void main(String[] args) {
+        port(getHerokuAssignedPort());
         staticFileLocation("/public");
 
         //view homepage:
