b3353c27b427affae59b0da1f2c91ea9d1f9ba51
JoyWambui <JoyWambui@github.com>
2021-04-26 14:00:49 +0300

    add heroes to a squad

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -1,14 +1,14 @@
 package models;
 
-import com.sun.org.apache.bcel.internal.generic.ANEWARRAY;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Objects;
 
 public class Squad {
     private String name;
     private String cause;
     private static ArrayList<Squad>  squads= new ArrayList<>();
+    private static HashSet<Hero> squadHeroes = new HashSet<>();
     private int id;
 
     public Squad(String name, String cause){
@@ -17,6 +17,19 @@ public class Squad {
         squads.add(this);
         this.id = squads.size();
 
+
+    }
+    public static Squad findSquadId(int id){
+        return squads.get(id-1);
+    }
+    public static ArrayList<Squad> getSquads() {
+        return squads;
+    }
+    public static void clearSquadsList(){
+        squads.clear();
+    }
+    public  void addToSquad(Hero hero){
+        squadHeroes.add(hero);
     }
 
     public String getName() {
@@ -27,21 +40,15 @@ public class Squad {
         return cause;
     }
 
-    public static ArrayList<Squad> getSquads() {
-        return squads;
-    }
-    public static void clearSquadsList(){
-        squads.clear();
-    }
-
-
     public int getId() {
         return id;
     }
-    public static Squad findSquadId(int id){
-       return squads.get(id-1);
+
+    public static HashSet<Hero> getSquadHeroes() {
+        return squadHeroes;
     }
-    public void update(String newName,String newCause){
+
+    public void update(String newName, String newCause){
         this.name = newName;
         this.cause = newCause;
     }
@@ -51,5 +58,20 @@ public class Squad {
 
     public String fullSquadInfo(){
         return getName() + " " + getCause();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj== null||obj.getClass()!= this.getClass()) return true;
+        Squad squad = (Squad) obj;
+        return this.getName().equals(squad.getName())&&
+                this.getId()==squad.getId()&&
+                this.getCause().equals(squad.getCause());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, cause, id);
     }
 }
